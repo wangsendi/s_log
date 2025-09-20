289a768bafc78a3288a8fc03191b06378ee51bbb
Wang Sen Di <wangsendi@vip.qq.com>
2025-09-21 03:53:54 +0800

    init s_log

--- logger.go
+++ logger.go
@@ -1,7 +1,6 @@
 package s_log
 
 import (
-	"context"
 	"io"
 	"log/slog"
 	"os"
@@ -41,54 +40,4 @@ func New(opts ...Option) error {
 		slog.SetDefault(log)
 	})
 	return err
-}
-
-// 日志函数
-func Debug(msg string, args ...any) {
-	if log != nil {
-		log.Debug(msg, args...)
-	}
-}
-
-func Info(msg string, args ...any) {
-	if log != nil {
-		log.Info(msg, args...)
-	}
-}
-
-func Warn(msg string, args ...any) {
-	if log != nil {
-		log.Warn(msg, args...)
-	}
-}
-
-func Error(msg string, args ...any) {
-	if log != nil {
-		log.Error(msg, args...)
-	}
-}
-
-// 带上下文的日志函数
-func DebugContext(ctx context.Context, msg string, args ...any) {
-	if log != nil {
-		log.DebugContext(ctx, msg, args...)
-	}
-}
-
-func InfoContext(ctx context.Context, msg string, args ...any) {
-	if log != nil {
-		log.InfoContext(ctx, msg, args...)
-	}
-}
-
-func WarnContext(ctx context.Context, msg string, args ...any) {
-	if log != nil {
-		log.WarnContext(ctx, msg, args...)
-	}
-}
-
-func ErrorContext(ctx context.Context, msg string, args ...any) {
-	if log != nil {
-		log.ErrorContext(ctx, msg, args...)
-	}
 }
